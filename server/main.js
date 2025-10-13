164d1f51dc5b9e4f31730e0ad33353835c089e40
Breno Pinna <brenopinna1008@gmail.com>
2025-10-13 12:39:49 -0300

    :rocket: deploy: change file reading logic

--- server/main.js
+++ server/main.js
@@ -1,5 +1,6 @@
 import express from "express"
 import dotenv from "dotenv"
+import { readFileSync } from "node:fs"
 
 dotenv.config({ quiet: true }) // a partir daqui, as variaveis de ambiente serao reconhecidas!
 
@@ -9,8 +10,7 @@ app.use(express.json())
 
 app.get("/comics", async (_, res) => {
   try {
-    const response = await fetch(process.env.DATABASE_BASEURL) // vai fazer uma requisicao ao servidor
-    const results = await response.json() // vai converter o corpo dessa resposta em json
+    const results = JSON.parse(readFileSync("./comics.json").toString())
     res.status(200).json(results)
   } catch (error) {
     res.status(400).json({
@@ -22,8 +22,9 @@ app.get("/comics", async (_, res) => {
 app.get("/comics/:id", async (req, res) => {
   try {
     const { id } = req.params // vai nos retornar os parâmetros da URL
-    const response = await fetch(`${process.env.DATABASE_BASEURL}/${id}`) // vai fazer uma requisicao especificamente a um elemento, se existir.
-    const results = await response.json() // vai converter o corpo dessa resposta em json
+    const results = JSON.parse(readFileSync("./comics.json").toString()).data.filter(
+      (el) => el.id === id,
+    )
     res.status(200).json(results)
   } catch (error) {
     res.status(400).json({
